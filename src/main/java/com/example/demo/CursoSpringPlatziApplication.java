2f166752e8ed78d5e60c3cb83b0e29152e27c4a5
unknown <afsanchezsa@unal.edu.co>
2022-02-16 13:39:06 -0500

    agregando un componente como dependencia

--- src/main/java/com/example/demo/CursoSpringPlatziApplication.java
+++ src/main/java/com/example/demo/CursoSpringPlatziApplication.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoSpringPlatziApplication {
+import com.example.demo.component.ComponentDependency;
 
+@SpringBootApplication
+public class CursoSpringPlatziApplication implements CommandLineRunner{
+	private ComponentDependency componentDependency;
+	
+	@Autowired
+	public CursoSpringPlatziApplication(ComponentDependency componentDependency) {
+		this.componentDependency=componentDependency;
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringPlatziApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		this.componentDependency.saludar();
+		
+	}
+	
 }
---
+++ src/main/java/com/example/demo/component/ComponentDependency.java
@@ -0,0 +1,5 @@
+package com.example.demo.component;
+
+public interface ComponentDependency {
+	void saludar();
+}
